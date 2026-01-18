182930ac11d5886b549f33df3b1b015bf36706e5
VinhHV28 <vinhhv28@gmail.com>
2026-01-18 14:04:13 +0700

    initialize admin

---
+++ admin/src/App.jsx
@@ -0,0 +1,15 @@
+import { Button, DatePicker } from "antd";
+
+function App() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Hello Ant Design 🚀</h1>
+      <Button type="primary">AntD Button</Button>
+      <br />
+      <br />
+      <DatePicker />
+    </div>
+  );
+}
+
+export default App;
