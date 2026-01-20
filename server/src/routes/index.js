144de0f1db7bcf1d930f39feccf8ed13a44fd36b
VinhHV28 <vinhhv28@gmail.com>
2026-01-20 18:44:32 +0700

    api login

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import categoryRoutes from "../modules/category/category.route.js";
 import productRoutes from "../modules/product/product.route.js";
 import userRoutes from "../modules/user/user.route.js";
+import authRoutes from "../modules/auth/auth.route.js";
 
 const router = Router();
 
@@ -9,5 +10,6 @@ const router = Router();
 router.use("/categories", categoryRoutes);
 router.use("/products", productRoutes);
 router.use("/users", userRoutes);
+router.use("/auth", authRoutes);
 
 export default router;
